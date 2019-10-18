51477086844167deff5f33e81689d525fd1db0b3
erimsengezer <erimsengezer@gmail.com>
2019-10-19 02:18:41 +0300

    initial commit

---
+++ TODO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TODO
+//
+//  Created by Erim Şengezer on 19.10.2019.
+//  Copyright © 2019 Erim Şengezer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
