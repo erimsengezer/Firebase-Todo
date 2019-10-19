8f02ac7f6fe669e12c61df26dc31f30a3c8aa992
erimsengezer <erimsengezer@gmail.com>
2019-10-19 03:17:40 +0300

    Add Login Screen

---
+++ TODO/LoginScreen.swift
@@ -0,0 +1,48 @@
+//
+//  LoginScreen.swift
+//  TODO
+//
+//  Created by Erim Şengezer on 19.10.2019.
+//  Copyright © 2019 Erim Şengezer. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    @State var email = ""
+    @State var password = ""
+    var body: some View {
+        VStack {
+            Text("Login")
+                .font(.title)
+                .fontWeight(.light)
+                .frame(width: UIScreen.main.bounds.width * 0.90, height: UIScreen.main.bounds.height * 0.10)
+            
+            TextField("Email Adress", text: $email)
+                .frame(width: UIScreen.main.bounds.width * 0.90, height: UIScreen.main.bounds.height * 0.02)
+                .padding()
+            TextField("Password", text: $password)
+                .frame(width: UIScreen.main.bounds.width * 0.90, height: UIScreen.main.bounds.height * 0.02)
+                .padding()
+            
+            AuthButton(buttonTitle: "Login")
+            
+//            Button(action: {
+//                print("Login")
+//            }) {
+//                Text("Login")
+//                    .foregroundColor(Color.gray)
+//            }
+//                .padding()
+//                .frame(width: UIScreen.main.bounds.width * 0.40, height: UIScreen.main.bounds.height * 0.05)
+            
+        
+        }
+    }
+}
+
+struct LoginScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginScreen()
+    }
+}
