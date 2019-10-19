6695e5aa0dd695b10d336e00c0144146a66652d2
erimsengezer <erimsengezer@gmail.com>
2019-10-19 17:26:19 +0300

    Add Twitter button style

---
+++ TODO/TwitterButton.swift
@@ -0,0 +1,26 @@
+//
+//  TwitterButton.swift
+//  TODO
+//
+//  Created by Erim Şengezer on 19.10.2019.
+//  Copyright © 2019 Erim Şengezer. All rights reserved.
+//
+
+import SwiftUI
+
+struct TwitterButton: View {
+    var body: some View {
+        Button(action: {
+            print("twitter")
+        }) {
+            Image("twittericon")
+                .foregroundColor(Color.gray)
+        }
+    }
+}
+
+struct TwitterButton_Previews: PreviewProvider {
+    static var previews: some View {
+        TwitterButton()
+    }
+}
